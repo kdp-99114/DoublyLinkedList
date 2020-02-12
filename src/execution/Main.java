1124bf374fc4ed4ce899641f9eb15d615561e039
loser <loser>
2020-02-12 17:34:46 +0530

    modify Main class

--- src/execution/Main.java
+++ src/execution/Main.java
@@ -1,7 +1,16 @@
 package execution;
 
+import definition.DoublyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        System.out.println(list);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
     }
 }

e18c122906186fbd5a0ff3b9863d154f3df29460
loser <loser>
2020-02-12 16:22:16 +0530

    init commit

---
+++ src/definition/DoublyLinkedList.java
@@ -0,0 +1,25 @@
+package definition;
+
+import adt.ListADT;
+
+public class DoublyLinkedList<E> implements ListADT<E> {
+    @Override
+    public boolean add(Object item) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(Object item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
